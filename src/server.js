86a796372671dc10ac8a934145434da78be29825
Laize Ferraz <laizeferraz@gmail.com>
2025-09-17 17:20:21 +1000

    save users in memory

--- src/server.js
+++ src/server.js
@@ -1,7 +1,26 @@
 import http from "node:http"
 
+const users = []
 const server = http.createServer((req, res) => {
-  return res.end("Hello world")
+  const {method, url} = req
+
+  if (method === 'GET' && url === '/users') {
+    return res
+      .setHeader('Content-Type', 'application/json')
+      .end(JSON.stringify(users))
+  }
+
+    if (method === 'POST' && url === '/users') {
+      users.push({
+        id: 1,
+        name: 'John Doe',
+        email: 'johndoe@email.com'
+      })
+
+      return res.end('New user created.')
+  }
+
+  return res.end('Hello World')
 })
 
 server.listen(3333)
