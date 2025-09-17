7b699456dc80a3739b8aa87517231a06a3f26732
Laize Ferraz <laizeferraz@gmail.com>
2025-09-17 19:34:53 +1000

    create database & add json middleware

--- src/server.js
+++ src/server.js
@@ -1,37 +1,32 @@
 import http from "node:http"
+import { json } from "./middlewares/json.js"
+import { Database } from "./database.js"
 
-const users = []
+const database = new Database()
+
 const server = http.createServer(async (req, res) => {
   const {method, url} = req
 
-  const buffers = []
-
-  for await (const chunk of req) {
-    buffers.push(chunk)
-  }
-
-  try {
-    req.body = JSON.parse(Buffer.concat(buffers).toString())
-  } catch {
-    req.body = null
-  }
+  await json(req, res)
 
   if (method === 'GET' && url === '/users') {
-    return res
-      .setHeader('Content-Type', 'application/json')
-      .end(JSON.stringify(users))
+    const users = database.select('users')
+
+    return res.end(JSON.stringify(users))
   }
 
-    if (method === 'POST' && url === '/users') {
-      const { name, email } = req.body
-      users.push({
-        id: 1,
-        name,
-        email
-      })
+  if (method === 'POST' && url === '/users') {
+    const { name, email } = req.body
+    const user = {
+      id: 1,
+      name,
+      email
+    }
 
-      return res.writeHead(201).end()
-  }
+    database.insert('users', user)
+
+    return res.writeHead(201).end()
+}
 
   return res.writeHead(404).end()
 })
