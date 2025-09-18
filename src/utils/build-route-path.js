ba910a50ad33fabfc3708e133f1aefffffb4b44d
Laize Ferraz <laizeferraz@gmail.com>
2025-09-18 18:28:06 +1000

    route with params (regex)

--- src/utils/build-route-path.js
+++ src/utils/build-route-path.js
@@ -1,5 +1,11 @@
 export function buildRoutePath(path) {
   const routeParametersRegex = /:([a-zA-Z]+)/g
 
-  console.log(Array.from(path.matchAll(routeParametersRegex)))
+  const paramsWithParams = path.replaceAll(routeParametersRegex, '(?<$1>[a-z0-9\-_]+)')
+
+  const pathRegex = new RegExp(`^${paramsWithParams}`)
+
+  //console.log(Array.from(path.matchAll(routeParametersRegex)))
+
+  return pathRegex
 }
